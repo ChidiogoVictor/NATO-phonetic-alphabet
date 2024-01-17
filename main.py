e2c78e441b1f6e19253400c525a6e4a694b74a5c
ChidiogoVictor <chidiogovictor10@gmail.com>
2024-01-17 21:38:42 +0100

    Used exception handling to optimize performance

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Keyword Method with iterrows()
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, type in only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
